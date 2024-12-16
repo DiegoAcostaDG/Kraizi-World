f410bf90aba359364f531706b4b0026b2d3cd3a4
Diego_Acosta <dacogon1003@g.educaand.es>
2024-12-16 14:41:21 +0100

    añadir

--- Prueba-Java/src/ClasePersonaje.java
+++ Prueba-Java/src/ClasePersonaje.java
@@ -1,3 +1,33 @@
 public class ClasePersonaje {
-    
+    public enum TipoClasePersonaje {
+        GUERRERO(100, 20),
+        PICARO(80, 25),
+        MAGO(60, 30);
+
+        @SuppressWarnings("FieldMayBeFinal")
+        private int vida;
+        @SuppressWarnings("FieldMayBeFinal")
+        private int ataque;
+
+        // Constructor
+        TipoClasePersonaje(int vida, int ataque) {
+            this.vida = vida;
+            this.ataque = ataque;
+        }
+
+        // Métodos para obtener los valores de vida y ataque
+        public int getVida() {
+            return vida;
+        }
+
+        public int getAtaque() {
+            return ataque;
+        }
+    }
+
+    public static void main(String[] args) {
+        for (TipoClasePersonaje clase : TipoClasePersonaje.values()) {
+            System.out.println(clase + ": Vida = " + clase.getVida() + ", Ataque = " + clase.getAtaque());
+        }
+    }
 }
--- Prueba-Java/src/Jugador.java
+++ Prueba-Java/src/Jugador.java
@@ -3,75 +3,71 @@ import java.util.Scanner;
 
 public class Jugador {
 
-    //Atributos
-    public String nombre;
-    public String clasePersonaje;
-    public String raza;
-    public int nivel;
+    // Atributos
+    private String nombre;
+    private String raza;
+    private int nivel;
+    private ClasePersonaje.TipoClasePersonaje clasePersonaje;
 
+    // Constructor
+    public Jugador(ClasePersonaje.TipoClasePersonaje clasePersonaje, int nivel, String nombre, String raza) {
+        this.clasePersonaje = clasePersonaje;
+        this.nivel = nivel;
+        this.nombre = nombre;
+        this.raza = raza;
+    }
+
+    // Métodos getter y setter
     public String getNombre() {
         return nombre;
     }
-
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-
-    public String getClasePersonaje() {
-        return clasePersonaje;
-    }
-
-
-    public void setClasePersonaje(String clasePersonaje) {
-        this.clasePersonaje = clasePersonaje;
-    }
-
-
     public String getRaza() {
         return raza;
     }
-
 
     public void setRaza(String raza) {
         this.raza = raza;
     }
 
-
     public int getNivel() {
         return nivel;
     }
-
 
     public void setNivel(int nivel) {
         this.nivel = nivel;
     }
 
-
-    public Jugador(String clasePersonaje, int nivel, String nombre, String raza) {
-        this.clasePersonaje = clasePersonaje;
-        this.nivel = nivel;
-        this.nombre = nombre;
-        this.raza = raza;
+    public ClasePersonaje.TipoClasePersonaje getClasePersonaje() {
+        return clasePersonaje;
     }
 
-    
+    public void setClasePersonaje(ClasePersonaje.TipoClasePersonaje clasePersonaje) {
+        this.clasePersonaje = clasePersonaje;
+    }
+
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Jugador jugador1 = new Jugador(null, 1, null, null);
+
         System.out.println("Introduce el nombre de tu personaje: ");
         String nombre = scanner.next();
-        jugador1.setNombre(nombre);
-        System.out.println("Introduce la clase de tu personaje: ");
-        String clase = scanner.next();
-        jugador1.setClasePersonaje(clase);
+
+        System.out.println("Introduce la clase de tu personaje (GUERRERO, PICARO, MAGO): ");
+        String clase = scanner.next().toUpperCase();
+        ClasePersonaje.TipoClasePersonaje tipoClasePersonaje = ClasePersonaje.TipoClasePersonaje.valueOf(clase);
+
         System.out.println("Introduce la raza de tu personaje: ");
         String raza = scanner.next();
-        jugador1.setRaza(raza);
 
-        System.out.println("Las características de tu personaje son : \nNombre:"+jugador1.getNombre()+"\nClase:"+ jugador1.getClasePersonaje()+ "\nRaza:"+jugador1.getRaza()+"\nNivel:"+jugador1.getNivel());
+        Jugador jugador1 = new Jugador(tipoClasePersonaje, 1, nombre, raza);
+
+        System.out.println("Las características de tu personaje son : \nNombre: " + jugador1.getNombre() + "\nClase: " + jugador1.getClasePersonaje() + "\nRaza: " + jugador1.getRaza() + "\nNivel: " + jugador1.getNivel());
+
         scanner.close();
     }
-
 }
